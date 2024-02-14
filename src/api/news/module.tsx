ba55b12a37440c1c58c229923f7eb4e5fec5be3b
ElizavetaRazumenko <razumenko99@mail.ru>
2024-02-14 22:31:02 +0300

    feat[step-2]: refactoring

--- src/api/news/module.tsx
+++ src/api/news/module.tsx
@@ -4,14 +4,10 @@ import { ArticleItem } from './types';
 
 export const fetchNews = async (
   section: string,
-): Promise<ArticleItem[] | undefined> => {
-  try {
-    const response = await fetch(
-      `${BASIC_URL}/${section}.json?api-key=${TOP_STORIES_API_KEY}`,
-    );
-    const newData = await response.json();
-    return convertArticles(newData);
-  } catch (error) {
-    console.error(`Fetch operation failed: ${error}`);
-  }
+): Promise<ArticleItem[]> => {
+  const response = await fetch(
+    `${BASIC_URL}/${section}.json?api-key=${TOP_STORIES_API_KEY}`,
+  );
+  const newData = await response.json();
+  return convertArticles(newData);
 };
